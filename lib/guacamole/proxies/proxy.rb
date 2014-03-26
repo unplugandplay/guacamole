cdc90dba4bcadb3e43e9dfd6876a6b1207deb99d
Dirk Breuer <dirk.breuer@gmail.com>
2014-03-26 14:32:10 +0100

    Guacamole::Proxy: Added some basic documentation

--- lib/guacamole/proxies/proxy.rb
+++ lib/guacamole/proxies/proxy.rb
@@ -1,11 +1,28 @@
 module Guacamole
   module Proxies
+    # This is the base class for the association proxies. Proxies are only
+    # needed for non-embedded relations between objects. Embedded objects are
+    # taken care of by Virtus.
+    #
+    # The `Proxy` class undefines most methods and passes them to the
+    # `@target`.  The `@target` will be a lambda which will lazy query the
+    # requested objects from the database.
+    #
+    # Concrete proxy classes are:
+    #
+    #  * {Guacamole::Proxies::ReferencedBy}: This will handle one-to-many associations
+    #  * {Guacamole::Proxies::References}: This will handle many-to-one associations
     class Proxy
       # We undefine most methods to get them sent through to the target.
       instance_methods.each do |method|
         undef_method(method) unless method =~ /(^__|^send|^object_id|^respond_to|^tap)/
       end
 
+      # Convenience method to setup the proxy. The subclasses need to care of creating
+      # the `target` correctly.
+      #
+      # @param [Object] base The class holding the reference. Currently not used.
+      # @param [#call] target The lambda for getting the required objects from the database.
       def init(base, target)
         @base = base
         @target = target
